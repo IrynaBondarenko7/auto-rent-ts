b32ecbc46b7b28820d1560f5a367450857d7cf4a
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-11-01 21:08:09 +0000

    added routing

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { lazy } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+
+const HomePage = lazy(() => import("./pages/Home"));
+const CatalogPage = lazy(() => import("./pages/Catalog"));
+const FavoritesPage = lazy(() => import("./pages/Favorites"));
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
